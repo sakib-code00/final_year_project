571e09466b6b162bb8f7427880f0f9356a94c5c6
AHN <ahndo7059@gmail.com>
2025-06-20 01:50:17 +0600

    Remove language and notification settings options from Profilesetting component

--- src/Components/Users/MainContent/Profilesetting.jsx
+++ src/Components/Users/MainContent/Profilesetting.jsx
@@ -13,33 +13,6 @@ const Profilesetting = () => {
         {/*--------- Setting Options ------------*/}
         <div className='flex flex-col gap-4'>
 
-          {/*--------- Option 1 ------------*/}
-          <div className='bg-gray-100 rounded-xl p-4 flex items-center justify-between gap-96 '>
-            <div className=' flex items-center justify-center gap-3'>
-              <TfiWorld className='text-3xl text-gray-700 font-bold'></TfiWorld>
-              <div>
-                <h2 className='text-xl font-bold text-gray-700'>Language</h2>
-                <p className='text-sm text-gray-400 '>Current: <span>English</span></p>
-              </div>
-            </div>
-            
-            <button className='text-base font-medium py-1 px-2 rounded-xl border border-gray-400'>Change</button>
-            
-          </div>
-          {/*--------- Option 1 ------------*/}
-
-          {/*--------- Option 2 ------------*/}
-          <div className='bg-gray-100 rounded-xl p-4 flex items-center justify-between'>
-            <div className=' flex items-center justify-center gap-3'>
-              <IoNotificationsOutline className='text-3xl text-gray-700 font-bold'></IoNotificationsOutline>
-              <div>
-                <h2 className='text-xl font-bold text-gray-700'>Notifications</h2>
-                <p className='text-sm text-gray-400 '>Manage your notification preferences</p>
-              </div>
-            </div>
-            <button className='text-base font-medium py-1 px-2 rounded-xl border border-gray-400'>Manage</button>
-          </div>
-          {/*--------- Option 2 ------------*/}
 
           {/*--------- Option 3 ------------*/}
           <div className='bg-gray-100 rounded-xl p-4 flex items-center justify-between'>
